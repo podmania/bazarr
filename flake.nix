d1d8ea82ca085fe628c0a92ca192f43da3b52935
bryce <bryce.hoehn@icloud.com>
2026-04-25 17:26:22 -0400

    init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Bazarr distroless image";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }: let
+    system = builtins.currentSystem;
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    packages.${system} = {
+      bazarr-image = pkgs.dockerTools.buildLayeredImage {
+        name = "bazarr";
+        tag = "latest";
+        contents = [ 
+          pkgs.bazarr
+          pkgs.cacert
+          pkgs.tzdata
+        ];
+        config = {
+          ExposedPorts = {
+            "6767/tcp" = {};
+          };
+          Volumes = {
+            "/config" = {};
+            "/data" = {};
+          };
+          # Tell Bazarr to use /config as its data directory
+          Cmd = [ "${pkgs.bazarr}/bin/Bazarr" "--config" "/config" "--no-update" "True" ];
+          # Distroless non‑root user
+          User = "1000";
+          WorkingDir = "/config";
+        };
+      };
+    };
+
+    # Expose the Sonarr version for CI workflows
+    bazarrVersion = pkgs.bazarr.version;
+
+    defaultPackage.${system} = self.packages.${system}.bazarr-image;
+  };
+}
